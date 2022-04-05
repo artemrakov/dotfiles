1cb3d85a420509f4d6c67fe875007313b09c994b
Artem Rakov <artem@rakov.me>
2022-04-05 05:47:34 +0300

    proper keybindings for snippets

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -32,3 +32,14 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+keymap("i", "<c-y>", "<cmd>lua require'luasnip'.expand_or_jump()<CR>", opts)
+keymap("s", "<c-y>", "<cmd>lua require'luasnip'.expand_or_jump()<CR>", opts)
+
+keymap("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
