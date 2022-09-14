7e8fee6b6a556a35889f8bcb997337143e27d015
Artem Rakov <artem@rakov.me>
2022-09-13 20:58:17 -0400

    keybindings

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -1,113 +1,110 @@
-local keymap = vim.keymap.set
+local bind = vim.keymap.set
 local silent = { silent = true }
 local opts = { noremap = true, silent = true }
 
 
-keymap("n", "//", ":nohlsearch<CR>", silent)
+bind("n", "//", ":nohlsearch<CR>", silent)
 
-keymap('n', '<leader>vr', ':so $MYVIMRC<CR>')
-keymap('n', '<leader>vl', ':vsp $MYVIMRC<CR>')
+bind('n', '<leader>vr', ':so $MYVIMRC<CR>')
+bind('n', '<leader>vl', ':vsp $MYVIMRC<CR>')
 
 -- Better window movement
-keymap("n", "<C-l>", "<C-w>l", silent)
-keymap("n", "<C-h>", "<C-w>h", silent)
-keymap("n", "<C-j>", "<C-w>j", silent)
-keymap("n", "<C-k>", "<C-w>k", silent)
+bind("n", "<C-l>", "<C-w>l", silent)
+bind("n", "<C-h>", "<C-w>h", silent)
+bind("n", "<C-j>", "<C-w>j", silent)
+bind("n", "<C-k>", "<C-w>k", silent)
 
-keymap("n", "gV", "`[v`]", silent)
+bind("n", "gV", "`[v`]", silent)
 
-keymap("n", "k", "gk", silent)
-keymap("n", "j", "gj", silent)
+bind("n", "k", "gk", silent)
+bind("n", "j", "gj", silent)
 
 -- for russian layout
-keymap("n", "л", "gk", silent)
-keymap("n", "о", "gj", silent)
+bind("n", "л", "gk", silent)
+bind("n", "о", "gj", silent)
 
 -- Move selected line / block of text in visual mode
--- keymap("x", "K", ":move '<-2<CR>gv-gv", silent)
--- keymap("x", "J", ":move '>+1<CR>gv-gv", silent)
+-- bind("x", "K", ":move '<-2<CR>gv-gv", silent)
+-- bind("x", "J", ":move '>+1<CR>gv-gv", silent)
 
 -- Keep visual mode indenting
-keymap("v", "<", "<gv", silent)
-keymap("v", ">", ">gv", silent)
-
--- Save file by CTRL-S
--- keymap("n", "<C-s>", ":w<CR>", silent)
--- keymap("i", "<C-s>", "<ESC> :w<CR>", silent)
+bind("v", "<", "<gv", silent)
+bind("v", ">", ">gv", silent)
 
 -- Buffers
-keymap("n", "gn", ":bn<CR>", silent)
-keymap("n", "gp", ":bp<CR>", silent)
-
--- Don't yank on visual paste
-keymap("v", "p", '"_dP', silent)
-
+bind("n", "gn", ":bn<CR>", silent)
+bind("n", "gp", ":bp<CR>", silent)
 
 -- Quickfix
--- keymap("n", "<leader>,", ":cp<CR>", silent)
--- keymap("n", "<leader>.", ":cn<CR>", silent)
+-- bind("n", "<leader>,", ":cp<CR>", silent)
+-- bind("n", "<leader>.", ":cn<CR>", silent)
 
 -- Toggle quicklist
 -- https://github.com/ecosse3/nvim/blob/master/lua/utils/init.lua
--- keymap("n", "<leader>q", "<cmd>lua require('utils').toggle_quicklist()<CR>", silent)
+-- bind("n", "<leader>q", "<cmd>lua require('utils').toggle_quicklist()<CR>", silent)
 
 -- Easyalign
--- keymap("n", "ga", "<Plug>(EasyAlign)", silent)
--- keymap("x", "ga", "<Plug>(EasyAlign)", silent)
+-- bind("n", "ga", "<Plug>(EasyAlign)", silent)
+-- bind("x", "ga", "<Plug>(EasyAlign)", silent)
 
 -- Refactor with spectre
-keymap("n", "<Leader>pr", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", silent)
-keymap("v", "<Leader>pr", "<cmd>lua require('spectre').open_visual()<CR>")
+-- bind("n", "<Leader>pr", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", silent)
+-- bind("v", "<Leader>pr", "<cmd>lua require('spectre').open_visual()<CR>")
+
+-- bind(
+--   "v",
+--   "<leader>rr",
+--   "<Esc><rmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+--   opts
+-- )
 
 -- LSP
-keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", silent)
-keymap("n", "gr", "<cmd>lua vim.lsp.buf.references({ includeDeclaration = false })<CR>", silent)
--- keymap("n", "<C-Space>", "<cmd>lua vim.lsp.buf.code_action()<CR>", silent)
-keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", silent)
-keymap("v", "<leader>ca", "<cmd>'<,'>lua vim.lsp.buf.range_code_action()<CR>", silent)
-keymap("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>", silent)
-keymap("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting({ async = true })<CR>", silent)
-keymap("v", "<leader>cf", "<cmd>'<.'>lua vim.lsp.buf.range_formatting()<CR>", silent)
-keymap("n", "<leader>cl", "<cmd>lua vim.diagnostic.open_float({ border = 'rounded', max_width = 100 })<CR>", silent)
-keymap("n", "K", "<cmd>lua vim.lsp.buf.signature_help()<CR>", silent)
-keymap("n", "]g", "<cmd>lua vim.diagnostic.goto_next({ float = { border = 'rounded', max_width = 100 }})<CR>", silent)
-keymap("n", "[g", "<cmd>lua vim.diagnostic.goto_prev({ float = { border = 'rounded', max_width = 100 }})<CR>", silent)
-
--- Comment Box
-keymap("n", "<leader>ac", "<cmd>lua require('comment-box').lbox()<CR>", silent)
-keymap("v", "<leader>ac", "<cmd>lua require('comment-box').lbox()<CR>", silent)
+bind("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", silent)
+bind("n", "gr", "<cmd>lua vim.lsp.buf.references({ includeDeclaration = false })<CR>", silent)
+-- bind("n", "<C-Space>", "<cmd>lua vim.lsp.buf.code_action()<CR>", silent)
+bind("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", silent)
+bind("v", "<leader>ca", "<cmd>'<,'>lua vim.lsp.buf.range_code_action()<CR>", silent)
+bind("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>", silent)
+bind("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting({ async = true })<CR>", silent)
+bind("v", "<leader>cf", "<cmd>'<.'>lua vim.lsp.buf.range_formatting()<CR>", silent)
+bind("n", "<leader>cl", "<cmd>lua vim.diagnostic.open_float({ border = 'rounded', max_width = 100 })<CR>", silent)
+bind("n", "K", "<cmd>lua vim.lsp.buf.signature_help()<CR>", silent)
+bind("n", "]g", "<cmd>lua vim.diagnostic.goto_next({ float = { border = 'rounded', max_width = 100 }})<CR>", silent)
+bind("n", "[g", "<cmd>lua vim.diagnostic.goto_prev({ float = { border = 'rounded', max_width = 100 }})<CR>", silent)
 
 -- Telescope
-keymap("n", "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<cr>", silent)
-keymap("n", "<leader>sf", "<cmd>lua require('telescope.builtin').find_files()<cr>", silent)
-keymap("n", "<leader>sb", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", silent)
-keymap("n", "<leader>sg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", silent)
-keymap("n", "<leader>sd", "<cmd>lua require('telescope.builtin').diagnostics()<cr>", silent)
-keymap("n", "<leader>sc", "<cmd>lua require('telescope.builtin').git_commits()<cr>", silent)
-keymap("n", "<leader>sr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", silent)
-keymap("n", "<leader>so", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>", silent)
-keymap("n", "<leader>sa", "<cmd>lua require('telescope.builtin').lsp_range_code_actions()<cr>", silent)
-keymap("n", "<leader>sh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", silent)
+bind("n", "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<cr>", silent)
+bind("n", "<leader>sf", "<cmd>lua require('telescope.builtin').find_files()<cr>", silent)
+bind("n", "<leader>sb", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>", silent)
+bind("n", "<leader>sg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", silent)
+bind("n", "<leader>sd", "<cmd>lua require('telescope.builtin').diagnostics()<cr>", silent)
+bind("n", "<leader>sc", "<cmd>lua require('telescope.builtin').git_commits()<cr>", silent)
+bind("n", "<leader>sr", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", silent)
+bind("n", "<leader>so", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<cr>", silent)
+bind("n", "<leader>sa", "<cmd>lua require('telescope.builtin').lsp_range_code_actions()<cr>", silent)
+bind("n", "<leader>sh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", silent)
 
---
-keymap('n', '<leader><leader>', ':NvimTreeToggle<CR>')
-keymap('n', '<C-n>', ':NvimTreeFindFile<CR>')
+bind('n', '<leader><leader>', ':NvimTreeToggle<CR>')
+bind('n', '<C-n>', ':NvimTreeFindFile<CR>')
 
 -- LuaSnip for snippets
-keymap("i", "<c-y>", "<cmd>lua require'luasnip'.expand_or_jump()<CR>", opts)
-keymap("s", "<c-y>", "<cmd>lua require'luasnip'.expand_or_jump()<CR>", opts)
-keymap("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
-keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
-keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
-keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+bind("i", "<c-y>", "<cmd>lua require'luasnip'.expand_or_jump()<CR>", opts)
+bind("s", "<c-y>", "<cmd>lua require'luasnip'.expand_or_jump()<CR>", opts)
+bind("i", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+bind("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
+bind("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+bind("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
 
 -- Dropbox
-keymap("n", "<leader>nd", ":e ~/Dropbox<cr>", silent)
-keymap("n", "<leader>nj", ":sp ~/Dropbox/notes/debugging_journal.txt<cr>", silent)
-keymap("n", "<leader>nc", ":sp ~/Dropbox/notes/coding-notes.txt<cr>", silent)
-keymap("n", "<leader>nn", ":sp ~/Dropbox/notes/programming_notes.txt<cr>", silent)
-keymap("n", "<leader>np", ":sp ~/Dropbox/work/shogun/project-notes.md<cr>", silent)
+bind("n", "<leader>nd", ":e ~/Dropbox<cr>", silent)
+bind("n", "<leader>nj", ":sp ~/Dropbox/notes/debugging_journal.txt<cr>", silent)
+bind("n", "<leader>nc", ":sp ~/Dropbox/notes/coding-notes.txt<cr>", silent)
+bind("n", "<leader>nn", ":sp ~/Dropbox/notes/programming_notes.txt<cr>", silent)
+bind("n", "<leader>np", ":sp ~/Dropbox/work/shogun/project-notes.md<cr>", silent)
 
+-- Comment Box
+-- bind("n", "<leader>ac", "<cmd>lua require('comment-box').lbox()<CR>", opts)
+-- bind("v", "<leader>ac", "<cmd>lua require('comment-box').lbox()<CR>", opts)
 
 -- bind('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", opts)
 -- bind('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", opts)
