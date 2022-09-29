61c0a9f47624a2e0be492d26a818922883d34391
Artem Rakov <artem@rakov.me>
2022-09-29 17:33:21 -0400

    updates

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -8,7 +8,6 @@ function M.run(use)
       'neovim/nvim-lspconfig',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
     },
@@ -62,8 +61,6 @@ function M.run(use)
           { name = 'luasnip' },
           { name = 'path' },
           { name = 'buffer' },
-          { name = 'cmdline' },
-          { name = 'path' },
         },
       }
     end,
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -84,6 +84,23 @@ function M.run(use)
           capabilities = capabilities,
         }
       end
+
+      -- Check if the config is already defined (useful when reloading this file)
+      local configs = require 'lspconfig.configs'
+      if not configs.barium then
+        configs.barium = {
+          default_config = {
+            cmd = { 'barium' };
+            filetypes = { 'brazil-config' };
+            root_dir = function(fname)
+              return lspconfig.util.find_git_ancestor(fname)
+            end;
+            settings = {};
+          };
+        }
+      end
+
+      lspconfig.barium.setup {}
     end
   }
 
